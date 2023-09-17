226a63490433481b5d9dd9987216ff6343ba8683
Bmeyer-git <braxtonlmeyer@gmail.com>
2023-09-16 19:16:39 -0600

    Course Home Page updates

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+
+
+let copyright = document.querySelector("#copyright");
+copyright.innerHTML = "©" + new Date().getFullYear();
+
+let modifyrecord = document.querySelector("#lastModified");
+modifyrecord.innerHTML = "Last Modification: " + document.lastModified;
