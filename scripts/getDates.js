93fca490362f163f42c2fc8c5989f61561479362
Bmeyer-git <braxtonlmeyer@gmail.com>
2023-09-21 22:56:06 -0600

    Renamed js file, added more js functionality

--- scripts/getDates.js
+++
@@ -1,7 +0,0 @@
-
-
-let copyright = document.querySelector("#copyright");
-copyright.innerHTML = "©" + new Date().getFullYear();
-
-let modifyrecord = document.querySelector("#lastModified");
-modifyrecord.innerHTML = "Last Modification: " + document.lastModified;
---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+
+
+let copyright = document.querySelector("#copyright");
+copyright.innerHTML = "©" + new Date().getFullYear();
+
+let modifyrecord = document.querySelector("#lastModified");
+modifyrecord.innerHTML = "Last Modification: " + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    main.classList.toggle("dark");
+    if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
