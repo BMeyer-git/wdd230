23e0eb1fcf0596a027ccba45b38527c02405af56
Bmeyer-git <braxtonlmeyer@gmail.com>
2023-09-30 19:37:33 -0600

    Added discover page, linked to home page

---
+++ chamber/scripts/visitTracker.js
@@ -0,0 +1,20 @@
+
+const msToDays = 84600000;
+
+const theDateToday = new Date();
+const today = Date.now();
+
+let lastVisit = window.localStorage.getItem("lastVisit") || null;
+
+const welcomeElement = document.querySelector("#welcome");
+
+if (lastVisit == null) {
+	welcomeElement.textContent = "Welcome! Let us know if you have any questions.";
+} else if ((today - lastVisit) / msToDays < 1) {
+	welcomeElement.textContent = "Back so soon! Awesome!";
+}
+else{
+    welcomeElement.textContent = "You last visited " + Math.floor((today - lastVisit) / msToDays) + " days ago.";
+}
+
+localStorage.setItem("lastVisit", today);
