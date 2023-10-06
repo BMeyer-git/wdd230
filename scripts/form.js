77dd6480e045a2d47a44065c73e93dfc18a5eb19
Bmeyer-git <braxtonlmeyer@gmail.com>
2023-10-05 21:33:08 -0600

    Added form page, split scripts for other pages

---
+++ scripts/form.js
@@ -0,0 +1,28 @@
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.display = "block";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+	}
+}
+
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("rating");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value ;
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,4 @@
-
-
-let copyright = document.querySelector("#copyright");
-copyright.innerHTML = "©" + new Date().getFullYear();
-
-let modifyrecord = document.querySelector("#lastModified");
-modifyrecord.innerHTML = "Last Modification: " + document.lastModified;
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('nav');
-
 const visitsDisplay = document.querySelector(".visits");
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-
-});
-
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-    main.classList.toggle("dark");
-    if (modeButton.textContent.includes("☑️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-
-		modeButton.textContent = "❎";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "☑️";
-	}
-});
-
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
